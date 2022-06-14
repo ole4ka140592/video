6887616492810e6168609aa059761ce892b46eff
ole4ka140592 <ole4ka140592@gmail.com>
2022-06-14 23:55:21 +0300

    add changes in type Dispatch  thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {Search} from "./components/Search";
 
 
 function App() {
-
     return (
         <div className="App">
             <Search/>
